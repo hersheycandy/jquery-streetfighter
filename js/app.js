569dd0ec193eb044e3e386a7ec41947fd390c69a
Matthew Prestifilippo <matthew.prestifilippo@1stdibs.com>
2016-06-23 19:52:14 -0600

    fix multiple Ryu's showing up

--- js/app.js
+++ js/app.js
@@ -23,12 +23,13 @@ $(document).ready(function() {
 		);
 	})
 	.mouseup(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
+		$('.ryu-ready').show(); // show ready (jumping) on mouse up
+		$('.ryu-throwing').hide(); // hide throw image
+		$('.ryu-still').hide();
 	});
 	function playhadouken () {
-		$('#hadouken-sound') [0].volume=0.5;
-		$('#hadouken-sound') [0].load();
-		$('#hadouken-sound') [0].play ();
+		$('#hadouken-sound')[0].volume=0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
 	}
 });
